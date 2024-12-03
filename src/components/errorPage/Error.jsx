6f8aef25083c9968be56df1cabd13f3bdc9663d1
Tauqir <tauqirkhann.11@gmail.com>
2024-12-03 22:47:27 +0530

    feat: add basic error page design

--- src/components/errorPage/Error.jsx
+++ src/components/errorPage/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./Error.module.css";
+import Header from "../utils/header/header";
+
 const Error = () => {
-  return;
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <Link to="/">
+          This is error page go back to home
+          <br /> 🤗
+        </Link>
+      </main>
+    </>
+  );
 };
 
 export default Error;
